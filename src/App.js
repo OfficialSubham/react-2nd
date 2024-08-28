6b00f4ac8aacf8eb5ae95f10f7c828ca234b6e59
Subham <subhamoffi@gmail.com>
2024-08-28 10:41:48 +0530

    Added About and Text Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import Textform from "./Components/Textform";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+     
+  
+
+      <div className="container">
+        <Textform />
+      </div>
+
+      <div className="container">
+        <About
+        // changeMode={changeMode}
+        // style={aboutStyle}
+        // setStyle={setAboutStyle}
+        />
+      </div>
+
+    </>
+    
   );
 }
 
