76ad827ba7a6b23a6e22a19eb274c982eea19479
Subham <subhamoffi@gmail.com>
2024-08-28 18:35:39 +0530

    Added the Buttons to Maked them interactive

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -1,17 +1,64 @@
-// import { useState } from "react"
+import { useState } from "react";
 import React from "react";
 
 export default function Textform() {
-    return (
-        <div className="mb-5 my-3">
+
+  const [text, setText] = useState("Enter The Text")
+
+  const clickToSmall = () => {
+    let newText = text.toLocaleLowerCase();
+    setText(newText)
+  }
+
+  const clickToCapital = () => {
+    let newText = text.toLocaleUpperCase();
+    setText(newText)
+  }
+
+  const clickToCopy = () => {
+    // let text = text;
+    navigator.clipboard.writeText(text)
+  }
+
+  const writeText = (e) => {
+    // console.dir(e.target.value);
+    setText(e.target.value)
+  }
+
+  return (
+    <>
+      <div className="mb-3 my-3">
         <label htmlFor="exampleFormControlTextarea1" className="form-label">
-            Enter Text Here
+          Customise Your Text Here
         </label>
         <textarea
           className="form-control"
           id="exampleFormControlTextarea1"
           rows="10"
+          value={text}
+          onChange={writeText}
         ></textarea>
       </div>
-    )
-};
+
+      <div className="container">
+        <button type="button" className="btn btn-primary mx-3 my-2" onClick={clickToSmall}>
+          Make the Text Smaller
+        </button>
+        <button type="button" className="btn btn-primary mx-3 my-2" onClick={clickToCapital}>
+          Make the Text Capital
+        </button>
+        <button type="button" className="btn btn-primary mx-3 my-2" onClick={clickToCopy}>
+          Copy the Text
+        </button>
+      </div>
+
+      <div className="container my-3">
+        <h3>Preview</h3>
+        <div>
+          {text}
+        </div>
+      </div>
+
+    </>
+  );
+}
